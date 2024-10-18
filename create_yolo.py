85998e322e99654ce343644af86399da0287e43c
jeonghyeonE <kookil2012@gmail.com>
2024-10-18 13:08:34 +0900

    Update create_yolo.py

--- create_yolo.py
+++ create_yolo.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 def train_yolo():
     model = YOLO("yolov8s.pt")
     model.train(data='Vehicle_Person_Recognition/data.yaml',  # YAML 파일 경로
-            epochs=40,           # 학습 에포크 수
+            epochs=50,           # 학습 에포크 수
             imgsz=640,           # 입력 이미지 크기
             batch=32,            # 배치 크기
             name='yolo_vehicle_person',  # 학습 결과 저장 폴더 이름
